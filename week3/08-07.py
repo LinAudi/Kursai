7f321ca1ef50edf86e18e8d478ffdcd47d3becee
username <full@email.com>
2024-08-07 18:23:29 +0300

    Bandymas1

---
+++ week3/08-07.py
@@ -0,0 +1,9 @@
+def spausdinti_vardus():
+    vardai = ["Jonas", "Kazys", "Antanas"]
+
+    for vardas in vardai:
+        print(vardas) #Funkcija aprasyta
+
+spausdinti_vardus()#Funkcijai iskviesti
+spausdinti_vardus()#Funkcijai iskviesti
+spausdinti_vardus()#Funkcijai iskviesti
